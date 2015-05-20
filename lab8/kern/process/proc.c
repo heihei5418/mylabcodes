23f8d9097e001adaec993d79163b42ce9d2ca95e
os_lab_user <you@example.com>
2015-05-20 20:29:18 +0800

    merge previous codes to lab8

--- lab8/kern/process/proc.c
+++ lab8/kern/process/proc.c
@@ -123,6 +123,29 @@ alloc_proc(void) {
      *     uint32_t lab6_priority;                     // FOR LAB6 ONLY: the priority of process, set by lab6_set_priority(uint32_t)
      */
     //LAB8:EXERCISE2 YOUR CODE HINT:need add some code to init fs in proc_struct, ...
+		proc->state = PROC_UNINIT;
+		proc->pid = -1;
+		proc->runs = 0;
+		proc->kstack = 0;
+		proc->need_resched = 0;
+		proc->parent = NULL;
+		proc->mm = NULL;
+		memset(&(proc->context), 0, sizeof(struct context));
+		proc->tf = NULL;
+		proc->cr3 = boot_cr3;
+		proc->flags = 0;
+		memset(proc->name, 0, PROC_NAME_LEN);
+		
+		proc->wait_state = 0;
+		proc->cptr = proc->optr = proc->yptr = NULL;
+		
+		proc->rq = NULL;
+		list_init(&proc->run_link);
+		proc->time_slice = 0;
+		skew_heap_init(&proc->lab6_run_pool);
+		proc->lab6_stride = 0;
+		proc->lab6_priority = 0;
+        	proc->filesp = NULL;
     }
     return proc;
 }
@@ -461,7 +484,39 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
 	*    update step 1: set child proc's parent to current process, make sure current process's wait_state is 0
 	*    update step 5: insert proc_struct into hash_list && proc_list, set the relation links of process
     */
-	
+    proc = alloc_proc();
+    if (proc == NULL) {
+        goto fork_out;
+    }
+
+    proc->parent = current;
+    assert(current->wait_state == 0);
+
+    if (setup_kstack(proc) != 0) {
+        goto bad_fork_cleanup_proc;
+    }
+    if (copy_files(clone_flags, proc) != 0) { //for LAB8
+        goto bad_fork_cleanup_kstack;
+    }
+    if (copy_mm(clone_flags, proc) != 0) {
+        goto bad_fork_cleanup_kstack;
+    }
+    copy_thread(proc, stack, tf);
+
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        proc->pid = get_pid();
+        hash_proc(proc);
+        set_links(proc);
+
+    }
+    local_intr_restore(intr_flag);
+
+    wakeup_proc(proc);
+
+    ret = proc->pid;
+
 fork_out:
     return ret;
 
